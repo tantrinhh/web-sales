307e5e7eba41d57d944b0cc519426e6c5c127303
tantrinhh <tantrinh0322@gmail.com>
2023-09-26 10:31:28 +0700

    feat:pages

---
+++ src/Components/Cart_Sidebar/Cart_Sidebar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cart_Sidebar = () => {
+  return <div>Cart_Sidebar</div>;
+};
+
+export default Cart_Sidebar;
---
+++ src/Components/Single_Product/Single_Product.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Single_Product = () => {
+  return <div>Single_Product</div>;
+};
+
+export default Single_Product;
