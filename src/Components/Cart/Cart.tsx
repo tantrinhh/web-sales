c9bb1e1f6657155ae896d8282a5f7d49fa8639fd
tantrinhh <tantrinh0322@gmail.com>
2023-10-09 09:44:05 +0700

    updata: Cart

--- src/Components/Cart/Cart.tsx
+++ src/Components/Cart/Cart.tsx
@@ -1,7 +1,66 @@
-import LayoutPage from "../Layout/Layout_Page";
+import { AiTwotoneDelete } from "react-icons/ai";
+import Asgaardsofa from "../../assets/cart/Asgaardsofa.png";
+const Main_Cart = () => {
+  return (
+    <>
+      <div className="flex justify-between my-20 mx-24">
+        <div>
+          <div className="flex px-32 py-10 justify-between bg-[#F9F1E7] font-semibold text-base text-[#000000] ">
+            <div className="mr-28 ml-10">Product</div>
+            <div className="mr-40">Price</div>
+            <div className="mr-14">Quantity</div>
+            <div>Subtatotal</div>
+          </div>
 
-const Cart = () => {
-  return <></>;
+          <div className="flex mt-10 items-center font-medium text-base ">
+            <div>
+              <img
+                src={Asgaardsofa}
+                className="bg-[#F9F1E7] rounded-xl"
+                alt=""
+              />
+            </div>
+            <div className="mr-20 ml-14 text-[#9F9F9F]">Asgaardsofa</div>
+            <div className="mr-[105px] text-[#9F9F9F]">Rs. 250,000.00</div>
+            <div>
+              <input
+                type="number" // Sử dụng
+                className="w-14 h-10  px-2 rounded-md border-0 py-0.5 text-[#000000] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div className="ml-14 mr-10">Rs. 250,000.00</div>
+            <div>
+              <AiTwotoneDelete
+                style={{ width: "20px", height: "20px", color: "#B88E2F" }}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="items-center text-center bg-[#F9F1E7] px-20 pb-24 pt-10 rounded">
+          <div className=" font-semibold text-[32px] leading-[48px] text-[#000000] mb-14">
+            Cart Totals
+          </div>
+          <div className="flex justify-between max-w-[256px]">
+            <div className="text-base font-medium text-[#000000]">Subtotal</div>
+            <div className="text-[#9F9F9F] text-base font-normal">
+              Rs. 250,000.00
+            </div>
+          </div>
+          <div className="flex justify-between max-w-[256px] my-6">
+            <div className="text-base font-medium text-[#000000]">Total</div>
+            <div className="font-mediun text-xl leading-[30px] text-[#B88E2F]">
+              Rs. 250,000.00
+            </div>
+          </div>
+          <div>
+            <button className="border border-black rounded-[15px] px-20 h-14 font-normal text-xl mt-2 ">
+              Check Out
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
-export default Cart;
+export default Main_Cart;
--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import LayoutPage from "../../Components/Layout/Layout_Page";
 import Sub_Cart from "../../Components/Cart/Sub_Cart";
+import Main_Cart from "../../Components/Cart/Cart";
 
 export const Cart = () => {
   return (
     <div className="bg-[#FFFFFF]">
       <LayoutPage>
         <Sub_Cart />
+        <Main_Cart />
       </LayoutPage>
     </div>
   );
