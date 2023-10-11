30a497212563ae85f502e38385b179bbf0527c51
Hoang <pmhoangtnvn@gmail.com>
2023-10-11 10:20:19 +0700

    fix: product discount

---
+++ src/Components/store.ts
@@ -0,0 +1,13 @@
+// store.ts
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './cartSlice';
+
+const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export default store;
